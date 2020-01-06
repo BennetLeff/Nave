1c60c8bfc74d1e87560e4414a1faa0dbf767a084
bennet <bennetleff@gmail.com>
2020-01-06 14:09:41 -0600

    Create InteractiveAudioThumbnail class

---
+++ Source/InteractiveAudioThumbnail.cpp
@@ -0,0 +1,11 @@
+/*
+  ==============================================================================
+
+    InteractiveAudioThumbnail.cpp
+    Created: 6 Jan 2020 2:08:51pm
+    Author:  Bennet Leff
+
+  ==============================================================================
+*/
+
+#include "InteractiveAudioThumbnail.h"
